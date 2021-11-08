9abd5ccbaf664d5eaf00439586ec8118c443b0e7
Zeel <zeeljsoni@gmail.com>
2021-11-08 11:26:29 -0800

    Assignment 2 Initial Version

---
+++ assignment2/server.c
@@ -0,0 +1,109 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <sys/wait.h>
+#define PORT 8080
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+    
+    //struct passwd *user_info = getpwnam("dell");
+    struct passwd *user_info = getpwnam("nobody");
+    printf("User ID: %u\n",user_info->pw_uid);
+    if(user_info == NULL){
+        perror("Failed to get user id of NOBODY");
+    }
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    if (bind(server_fd, (struct sockaddr *)&address,
+	sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    // valread = read(new_socket, buffer, 1024);
+    // printf("Read %d bytes: %s\n", valread, buffer);
+    // send(new_socket, hello, strlen(hello), 0);
+    // printf("Hello message sent\n");
+
+    int pid = fork();
+    if(pid==-1){
+        perror("Error in creating child process");
+        exit(EXIT_FAILURE);
+    }
+    else if(pid==0){
+        printf("FROM CHILD: Inside Child Process\n");
+        int status = setuid(user_info->pw_uid);
+        if(status<0){
+            printf("FROM CHILD: Privilage is not given.");
+            //perror("Error in setting user id");
+            //exit(EXIT_FAILURE);
+        }
+
+        //Re-exec server's child process after forking
+        char *args[] = {"server.exe", (char *) &server_fd};
+        if(execv("server.exe", args) < 0){
+            perror("ERROR");
+            exit(EXIT_FAILURE);
+        }
+        server_fd = (int) * args[1];
+
+        if((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen))<0){
+            perror("ACCEPT");
+            exit(EXIT_FAILURE);
+        }
+
+        valread = read(new_socket, buffer, 1024);
+        printf("FROM CHILD: Read %d bytes: %s\n", valread, buffer);
+        send(new_socket, hello, strlen(hello), 0);
+        printf("FROM CHILD: Hello message sent from user Nobody and new Process.\n");
+        
+    }
+    else if(pid>0){
+        wait(NULL);
+    }
+
+    return 0;
+}
